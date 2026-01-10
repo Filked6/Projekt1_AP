6fc8aa29b296b6892eb1da76c159171a213a3454
Filip <filip.kedzior6@gmail.com>
2026-01-10 21:53:43 +0100

    Poprawione

--- Final_boss.py
+++ Final_boss.py
@@ -2,6 +2,8 @@ import arcpy
 from arcpy.sa import *
 import os
 
+from dask.bag.core import accumulate_part
+
 ########################
 # Zmienne środowiskowe #
 ########################
@@ -14,27 +16,28 @@ arcpy.env.cellSize = 10
 # Ścieżki do danych do projektu #
 #################################
 bdot10k_data = r"C:\Studia\Sezon_3\Analizy_przestrzenne\Projekt\projekt1\Projekt1_AP_POPRAWNY\Projekt1_AP\3214_SHP"
-project_path = r"C:\Studia\Sezon_3\Analizy_przestrzenne\Projekt\projekt1\Projekt1_AP_POPRAWNY\Projekt1_AP\Projekt1_AP.aprx"
 acceptable_file_names = ("budynek", "drogi", "lasy", "rezerwat1", "rezerwat2", "woda", "rzeka")
 
+project_path = r"C:\Studia\Sezon_3\Analizy_przestrzenne\Projekt\projekt1\Projekt1_AP_POPRAWNY\Projekt1_AP\Projekt1_AP.aprx"
 aprx = arcpy.mp.ArcGISProject(project_path)
-mapa = aprx.listMaps()[0]
 
 ###########
 # Funkcje #
 ###########
-def add_shp_files_to_arcgis(shp_folder, mapp, startings):
+def import_shp_to_gdb(shp_folder, gdb, startings):
     folder = os.listdir(shp_folder)
     for shp in folder:
-        if shp.lower().startswith(startings) and shp.endswith(".shp"):
+        if shp.startswith(startings) and shp.endswith(".shp"):
             full_path = os.path.join(shp_folder, shp)
-            print(f"Dodaję: {shp}")
-            mapp.addDataFromPath(full_path)
 
-    aprx.save()
+            out_name = os.path.splitext(shp)[0]
+            out_path = os.path.join(gdb, out_name)
+
+            print(f"Importuję do GDB: {shp} -> {out_name}")
+            arcpy.management.CopyFeatures(full_path, out_path)
 
 #####################
 # Wywołania funkcji #
 #####################
-#Tą funkcję się uruchamia tylko za pierwszym razem, gdy nie mamy plików shp w ArcGisie
-#add_shp_files_to_arcgis(bdot10k_data, mapa, acceptable_file_names)
+#Tą funkcję się uruchamia tylko za pierwszym razem, gdy nie mamy plików shp w geobazie
+#import_shp_to_gdb(bdot10k_data, arcpy.env.workspace, acceptable_file_names)
