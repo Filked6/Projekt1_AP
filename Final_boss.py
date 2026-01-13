07cc7d746d11ed44b34e908b2c42cb4256b806c2
Filip <filip.kedzior6@gmail.com>
2026-01-13 21:05:14 +0100

    a

--- Final_boss.py
+++ Final_boss.py
@@ -53,7 +53,7 @@ def distance_from_water_raster(min_val = 100, mean_val = 300, max_val = 1000):
     arcpy.management.Delete("rzeka_a")
     arcpy.management.Delete("distance_raster")
 
-def distance_from_buildings_raster():
+def distance_from_buildings_raster(min_from_building = 150, better = 1500 ):
     buildings = "budynek"
 
     condition = "\"FOBUD\" = 'budynki mieszkalne'"
@@ -61,13 +61,58 @@ def distance_from_buildings_raster():
     distance_raster = DistanceAccumulation(residental)
     distance_raster.save("distance_raster_temp")
 
-    f = FuzzyLinear(150, 1500)
+    f = FuzzyLinear(min_from_building, better)
     fuzzy_raster = FuzzyMembership(distance_raster, f)
     fuzzy_raster.save("distance_raster_residental")
 
     #Usuwanie tego co jest po drodze, jeśli potrzebne to trza zakomentować i będzie git
     arcpy.management.Delete("mieszkalne")
     arcpy.management.Delete("distance_raster_temp")
+
+def distance_from_forest_raster(min_from_forest = 15, better = 100):
+    forest = "lasy"
+    protected1 = "rezerwat1"
+    protected2 = "rezerwat2"
+
+    land_cover = arcpy.management.Merge([forest, protected1, protected2], "pokrycie_cale")
+    distance_raster = DistanceAccumulation(land_cover)
+    distance_raster.save("distance_raster_land_cover_temp")
+
+    f = FuzzyLinear(min_from_forest, better)
+    fuzzy_raster = FuzzyMembership(distance_raster, f)
+    fuzzy_raster.save("distance_raster_land_cover")
+
+    # Usuwanie tego co jest po drodze, jeśli potrzebne to trza zakomentować i będzie git
+    arcpy.management.Delete("pokrycie_cale")
+    arcpy.management.Delete("distance_raster_land_cover_temp")
+
+def road_availability():
+    road = "drogi"
+    column_values1 = ["żwir", "tłuczeń"]
+    column_values2 = ["kostka kamienna", "bruk", "kostka prefabrykowana"]
+
+    column_names = [f.name.upper() for f in arcpy.ListFields(road)]
+    if "POPULATION" not in column_names:
+        arcpy.management.AddField(road,"POPULATION", "SHORT")
+        with arcpy.da.UpdateCursor(road, ["MATE_NAWIE", "POPULATION"]) as cursor:
+            for row in cursor:
+                material = row[0]
+                if material is None:
+                    row[1] = 0
+                elif material == "grunt naturalny":
+                    row[1] = 0
+                elif material in column_values1:
+                    row[1] = 1
+                elif material in column_values2:
+                    row[1] = 2
+                else:
+                    row[1] = 3
+                cursor.updateRow(row)
+
+    raw_raster = LineDensity(road, "POPULATION", cell_size=20)
+    max_val = raw_raster.maximum
+    normalized_raster = raw_raster / max_val
+    normalized_raster.save("density_normalized_0_1")
 
 #####################
 # Wywołania funkcji #
@@ -81,3 +126,5 @@ def distance_from_buildings_raster():
 #-maksymalna akceptowalna odległość od wody
 #distance_from_water_raster()
 #distance_from_buildings_raster()
+#distance_from_forest_raster()
+road_availability()
